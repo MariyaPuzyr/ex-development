af28e2783eb93865780c97641d73a90cf5c58026
MariyaPuzyr <mariyapyzur@gmail.com>
2020-01-24 11:36:08 +0200

    add news template

--- tpl-news.php
+++ tpl-news.php
@@ -1,35 +1,32 @@
+<?php
+/*
+
+Template Name: News page template
+
+*/
+?>
 <div class="currency-reserves" id="content">
   <div class="d-flex align-items-center mb-2 position-relative"><a class="back-link" href="#"></a>
     <h2 class="sidebar-title">Новости</h2>
   </div>
+    <?php
+    $args = unserialize(stripslashes($_POST['query']));
+    $args['post_status'] = 'publish';
+    $query = new WP_Query($args);
+    ?>
   <div class="news-list">
-    <div class="news-list__item meganews new" data-toggle="modal" data-target="#news">
-      <div class="news-list__item--date">17 мая 2017</div>
-      <div class="news-list__item--link" data-toggle="modal" data-target="#news">Добавили переводы в Райффайзен банк.</div>
-    </div>
-    <div class="news-list__item new" data-toggle="modal" data-target="#news">
-      <div class="news-list__item--date">16 мая 2017</div>
-      <div class="news-list__item--link" data-toggle="modal" data-target="#news">Открылись Skype и Telegram аккаунты. Операторы работают круглосуточно и без выходных.</div>
-    </div>
-    <div class="news-list__item new" data-toggle="modal" data-target="#news">
-      <div class="news-list__item--date">16 мая 2017</div>
-      <div class="news-list__item--link" data-toggle="modal" data-target="#news">Новое направление - Сбербанк.</div>
-    </div>
-    <div class="news-list__item new action" data-toggle="modal" data-target="#news">
-      <div class="news-list__item--date">17 мая 2017</div>
-      <div class="news-list__item--link" data-toggle="modal" data-target="#news">Акция! Умножаем партнерский процент в декабре на 2!</div>
-    </div>
-    <div class="news-list__item" data-toggle="modal" data-target="#news">
-      <div class="news-list__item--date">17 мая 2017</div>
-      <div class="news-list__item--link" data-toggle="modal" data-target="#news">Существенно увеличили резервы Бикойнов, Qiwi, Advanced Cash.</div>
-    </div>
-    <div class="news-list__item action" data-toggle="modal" data-target="#news">
-      <div class="news-list__item--date">17 мая 2017</div>
-      <div class="news-list__item--link" data-toggle="modal" data-target="#news">Делаем 10% скидку на обмен за 2х привлеченных активных пользователей!</div>
-    </div>
-    <div class="news-list__item" data-toggle="modal" data-target="#news">
-      <div class="news-list__item--date">17 мая 2017</div>
-      <div class="news-list__item--link" data-toggle="modal" data-target="#news">Открылось новое направление Альфабанк. Узнать детали.</div>
-    </div>
+    <?php
+      if ($query->have_posts()):
+        while ($query->have_posts()): $query->the_post();
+            ?>
+          <div class="news-list__item new" data-toggle="modal" data-target="#news">
+            <div class="news-list__item--date"><?php echo get_the_date(); ?></div>
+            <div class="news-list__item--link" data-toggle="modal" data-target="#news"><?php the_content(); ?></div>
+          </div>
+        <?php
+        endwhile;
+      endif;
+    wp_reset_postdata();
+    ?>
   </div>
 </div>
