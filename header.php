a7c9073a619fd2f1a9106a945aea7bec56aad6a1
MariyaPuzyr <mariyapyzur@gmail.com>
2020-01-24 11:38:42 +0200

    remove top bar and top menu from header.php

--- header.php
+++ header.php
@@ -27,80 +27,6 @@ $plugin = get_plugin_class();
 	$h_change = get_theme_option('h_change', array('fixheader','phone','icq','skype','email','linkhead','telegram','viber','whatsapp','jabber'));
 	?>
 
-	<!-- top bar -->
-  <div class="topbar_wrap" <?php if ($h_change['fixheader'] == 1){ ?>id="fix_div"<?php } ?>>
-    <div class="topbar_ins" <?php if ($h_change['fixheader'] == 1){ ?>id="fix_elem"<?php } ?>>
-      <div class="topbar">
-
-          <?php the_lang_list('tolbar_lang'); ?>
-
-          <?php if ($h_change['icq']) { ?>
-            <div class="topbar_icon icq">
-                <?php echo get_contact($h_change['icq'], 'icq'); ?>
-            </div>
-          <?php } ?>
-
-          <?php if ($h_change['telegram']) { ?>
-            <div class="topbar_icon telegram">
-                <?php echo get_contact($h_change['telegram'], 'telegram'); ?>
-            </div>
-          <?php } ?>
-
-          <?php if ($h_change['viber']) { ?>
-            <div class="topbar_icon viber">
-                <?php echo get_contact($h_change['viber'], 'viber'); ?>
-            </div>
-          <?php } ?>
-
-          <?php if ($h_change['whatsapp']) { ?>
-            <div class="topbar_icon whatsapp">
-                <?php echo get_contact($h_change['whatsapp'], 'whatsapp'); ?>
-            </div>
-          <?php } ?>
-
-          <?php if ($h_change['jabber']) { ?>
-            <div class="topbar_icon jabber">
-                <?php echo get_contact($h_change['jabber'], 'jabber'); ?>
-            </div>
-          <?php } ?>
-
-          <?php if ($h_change['skype']) { ?>
-            <div class="topbar_icon skype">
-                <?php echo get_contact($h_change['skype'], 'skype'); ?>
-            </div>
-          <?php } ?>
-
-          <?php if ($h_change['email']) { ?>
-            <div class="topbar_icon email">
-                <?php echo get_contact($h_change['email'], 'email'); ?>
-            </div>
-          <?php } ?>
-
-          <?php if ($h_change['phone']) { ?>
-            <div class="topbar_icon phone">
-                <?php echo get_contact($h_change['phone'], 'phone'); ?>
-            </div>
-          <?php } ?>
-
-          <?php if ($user_id) { ?>
-            <a href="<?php echo get_pn_action('logout', 'get'); ?>"
-               class="toplink toplink_exit"><?php _e('Exit', 'pntheme'); ?></a>
-            <a href="<?php echo $plugin->get_page('account'); ?>"
-               class="toplink toplink_userlogin"><?php echo get_caps_name($ui->user_login); ?></a>
-          <?php } else { ?>
-            <a href="<?php echo $plugin->get_page('register'); ?>"
-               class="toplink toplink_signup js_window_join"><?php _e('Sign up', 'pntheme'); ?></a>
-            <a href="<?php echo $plugin->get_page('login'); ?>"
-               class="toplink toplink_signin js_window_login"><?php _e('Sign in', 'pntheme'); ?></a>
-          <?php } ?>
-
-        <div class="clear"></div>
-      </div>
-    </div>
-  </div>
-	<!-- end top bar -->
-
-
   <!-- add my own header -->
   <header class="header"><a class="logo" href="/"><img src="<?php echo $or_template_directory; ?>/img/svg/logo.svg" alt=""><span>exal.money</span></a>
     <div class="header-menu">
@@ -234,7 +160,7 @@ $plugin = get_plugin_class();
               <ul class="links-list">
                 <li><a href="exchange.html">Обмен</a></li>
                 <li><a class="load-page" href="#" data-link="<?php echo $or_template_directory . '/tpl-currency-reserves.php'; ?>">Резервы валют</a></li>
-                <li><a class="load-page" href="#" data-link="<?php echo $or_template_directory . '/tpl-news.php'; ?>">Новости</a></li>
+                <li><a class="load-page" href="#" data-link="news-list">Новости</a></li>
                 <li><a href="#">О сервисе</a></li>
                 <li><a href="affiliate-program.html">Партнерская программа</a></li>
               </ul>
@@ -258,63 +184,6 @@ $plugin = get_plugin_class();
     </div>
   </header>
   <!-- add my own header end -->
-
-	<!-- top menu -->
-	<div class="tophead_wrap" <?php if($h_change['fixheader'] == 2){ ?>id="fix_div"<?php } ?>>
-		<div class="tophead_ins" <?php if($h_change['fixheader'] == 2){ ?>id="fix_elem"<?php } ?>>
-			<div class="tophead">
-
-				<div class="logoblock">
-					<div class="logoblock_ins">
-						<?php if($h_change['linkhead'] == 1 and !is_front_page() or $h_change['linkhead'] != 1){ ?>
-							<a href="<?php echo get_site_url_ml(); ?>">
-						<?php } ?>
-
-							<?php
-							$logo = get_logotype();
-							$textlogo = get_textlogo();
-							if($logo){
-							?>
-								<img src="<?php echo $logo; ?>" alt="" />
-							<?php } elseif($textlogo){ ?>
-								<?php echo $textlogo; ?>
-							<?php } else {
-								$textlogo = str_replace(array('http://','https://','www.'),'',get_site_url_or());
-							?>
-								<?php echo get_caps_name($textlogo); ?>
-							<?php } ?>
-
-						<?php if($h_change['linkhead'] == 1 and !is_front_page() or $h_change['linkhead'] != 1){ ?>
-							</a>
-						<?php } ?>
-					</div>
-				</div>
-
-				<div class="topmenu js_menu">
-					<?php
-					if($user_id){
-						$theme_location = 'the_top_menu_user';
-					} else {
-						$theme_location = 'the_top_menu';
-					}
-					wp_nav_menu(array(
-						'sort_column' => 'menu_order',
-						'container' => 'div',
-						'container_class' => 'menu',
-						'menu_class' => 'hmenu',
-						'menu_id' => '',
-						'depth' => '3',
-						'fallback_cb' => 'no_menu',
-						'theme_location' => $theme_location
-					));
-					?>
-					<div class="clear"></div>
-				</div>
-					<div class="clear"></div>
-			</div>
-		</div>
-	</div>
-	<!-- end top menu -->
 
 	<div class="wrapper">
 
