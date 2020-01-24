bb1b29ee3de4840af2482cdd284ed4d0575dc6b8
MariyaPuzyr <mariyapyzur@gmail.com>
2020-01-24 18:13:32 +0200

    fix news template

--- tpl-news.php
+++ tpl-news.php
@@ -4,6 +4,8 @@
 Template Name: News page template
 
 */
+$currentDay = date("m.d.y");
+$postDay =  get_the_date();
 ?>
 <div class="currency-reserves" id="content">
   <div class="d-flex align-items-center mb-2 position-relative"><a class="back-link" href="#"></a>
@@ -18,13 +20,22 @@ Template Name: News page template
     <?php
       if ($query->have_posts()):
         while ($query->have_posts()): $query->the_post();
-            ?>
-          <div class="news-list__item new" data-toggle="modal" data-target="#news">
+      ?>
+      <?php if (get_the_date() == $currentDay) {?>
+          <div class="news-list__item meganews" data-toggle="modal" data-target="#news">
             <div class="news-list__item--date"><?php echo get_the_date(); ?></div>
-            <div class="news-list__item--link" data-toggle="modal" data-target="#news"><?php the_content(); ?></div>
+            <div class="news-list__item--link" data-toggle="modal" data-target="#news"><?php the_excerpt(); ?></div>
           </div>
-        <?php
+        <?php } else {
+        ?>
+    <div class="news-list__item" data-toggle="modal" data-target="#news">
+      <div class="news-list__item--date"><?php echo get_the_date(); ?></div>
+      <div class="news-list__item--link" data-toggle="modal" data-target="#news"><?php the_excerpt(); ?></div>
+    </div>
+    <?php
+            }
         endwhile;
+
       endif;
     wp_reset_postdata();
     ?>
